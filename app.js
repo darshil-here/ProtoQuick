f44a52fb2c91fd131d97f5e2834e5be826674b6b
Darshil Prajapati <darshil.0406@gmail.com>
2023-01-01 23:28:12 +0530

    tried other ways

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ import img2 from "./public/ss.png"
 mergeImages([img1, img2]).then(
   (b64) => (document.getElementById("img").src = b64)
 );
+
+//i don't understand what to do next, will continue tommorow
